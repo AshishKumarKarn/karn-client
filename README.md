089786e1ad5ac4d42b187ecb907bbb1e38e1d34c
Ashish Karn <ashishkrkarn@gmail.com>
2023-04-24 01:56:23 +0530

    Dynamically update config file, updating to latest cloud jars

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Client application based on spring cloud
 
 ## It uses karn-cloud-config-server running on port 8001 which in turn fetches the properties from git location
 ### https://github.com/AshishKumarKarn/cloud-config-server-files/      config-data     /karn-client.<extension>
+
+
+# To refresh the property file (config file from cloud) without restarting the application
+RUN: `curl -H 'Content-Type: application/json' -X POST  http://localhost:8002/actuator/refresh`
--- src/main/java/karn/cloud/karnclient/controller/LuckyWordController.java
+++ src/main/java/karn/cloud/karnclient/controller/LuckyWordController.java
@@ -1,14 +1,16 @@
 package karn.cloud.karnclient.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RefreshScope
 public class LuckyWordController {
 
-  @Value("${lucky-word}") String luckyWord;
+  @Value("${lucky-word}")
+  private String luckyWord;
 
   @GetMapping("/lucky-word")
   public String showLuckyWord() {
